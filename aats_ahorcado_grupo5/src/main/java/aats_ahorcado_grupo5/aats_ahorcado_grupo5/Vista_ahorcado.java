6002d3dc0c659b09d5c4697e12ec757e745f289f
Alejandro <alejandro.carayol@gmail.com>
2023-10-26 16:51:16 +0200

    Abrir ventana centrada

--- aats_ahorcado_grupo5/src/main/java/aats_ahorcado_grupo5/aats_ahorcado_grupo5/Vista_ahorcado.java
+++ aats_ahorcado_grupo5/src/main/java/aats_ahorcado_grupo5/aats_ahorcado_grupo5/Vista_ahorcado.java
@@ -7,14 +7,18 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class Vista_ahorcado extends JFrame {
-
+	
+	private final int FRAME_WIDTH = 800;
+	private final int FRAME_HEIGHT = 600;
 	private static final long serialVersionUID = 1L;
+	
 	private JPanel contentPane;
 
 
 	public Vista_ahorcado() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
