7f6224c13ad1ac2fc0499184d6f1227a7f633eaa
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-10-26 16:02:23 +0200

    first commit

---
+++ aats_ahorcado_grupo5/src/main/java/aats_ahorcado_grupo5/aats_ahorcado_grupo5/App.java
@@ -0,0 +1,13 @@
+package aats_ahorcado_grupo5.aats_ahorcado_grupo5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ aats_ahorcado_grupo5/src/test/java/aats_ahorcado_grupo5/aats_ahorcado_grupo5/AppTest.java
@@ -0,0 +1,20 @@
+package aats_ahorcado_grupo5.aats_ahorcado_grupo5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
