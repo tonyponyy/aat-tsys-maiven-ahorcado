b763c160308b61ff148c94ae014c0ce0512b0317
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-10-26 16:29:07 +0200

    second commit

---
+++ aats_ahorcado_grupo5/src/main/java/aats_ahorcado_grupo5/aats_ahorcado_grupo5/Main_app.java
@@ -0,0 +1,12 @@
+package aats_ahorcado_grupo5.aats_ahorcado_grupo5;
+
+
+public class Main_app {
+	public static void main(String[] args) {
+		try {
+			Vista_ahorcado frame = new Vista_ahorcado();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
--- aats_ahorcado_grupo5/src/main/java/aats_ahorcado_grupo5/aats_ahorcado_grupo5/Vista_ahorcado.java
+++ aats_ahorcado_grupo5/src/main/java/aats_ahorcado_grupo5/aats_ahorcado_grupo5/Vista_ahorcado.java
@@ -6,19 +6,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class vista_ahorcado extends JFrame {
+public class Vista_ahorcado extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 
-	public vista_ahorcado() {
+	public Vista_ahorcado() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		setVisible(true);
 	}
 
 }
