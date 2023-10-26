9c9ec0ab4c305d2ae3311ee878076895826eb897
Alejandro <alejandro.carayol@gmail.com>
2023-10-26 18:44:03 +0200

    primer commit

--- aats_ahorcado_grupo5/src/main/java/aats_ahorcado_grupo5/aats_ahorcado_grupo5/Menu.java
+++ aats_ahorcado_grupo5/src/main/java/aats_ahorcado_grupo5/aats_ahorcado_grupo5/Menu.java
@@ -3,5 +3,17 @@ package aats_ahorcado_grupo5.aats_ahorcado_grupo5;
 import javax.swing.JPanel;
 
 public class Menu extends JPanel{
-
+	
+	private final int PANEL_WIDTH = 400;
+	private final int PANEL_HEIGHT = 240;
+	private final int BUTTON_WIDTH = 50;
+	private final int BUTTON_HEIGHT = 25;
+	
+	public Menu() {
+		setLayout(null);
+		setSize(PANEL_WIDTH, PANEL_HEIGHT);
+		setLocation(10, 360);
+		
+		
+	}
 }
